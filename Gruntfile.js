8832ba1faeba56c94a73131c9b715e5cc748ab25
Miguel Palacio <migpalg@gmail.com>
2020-02-02 20:34:44 -0500

    Added build to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,46 @@ module.exports = function(grunt) {
         src: 'src/js/app.js',
         dest: 'dist/js/app.min.js'
       }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1,
+      },
+      target: {
+        files: {
+          'dist/css/styles.min.css': ['src/css/normalize.css', 'src/css/styles.css'],
+        }
+      }
+    },
+    replace: {
+      target: {
+        options: {
+          patterns: [
+            {
+              match: /\.js\"/g,
+              replacement: '.min.js"',
+            },
+            {
+              match: /<link rel="stylesheet" href="css\/normalize\.css">/,
+              replacement: '',
+            },
+            {
+              match: /\.css"/,
+              replacement: '.min.css"',
+            }
+          ],
+        },
+        files: [
+          {expand: true, flatten: true, src: ['src/*.html'], dest: 'dist/'}
+        ]
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-replace');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin', 'replace']);
 }
